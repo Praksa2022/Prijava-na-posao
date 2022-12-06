c93efab21c78a274598a1c18a79952be1dacd598
SEJO <sead.bander@gmail.com>
2022-12-06 18:42:17 +0100

    initial commit

---
+++ src/main/java/com/itacademy/AttendanceApp/AttendanceAppApplication.java
@@ -0,0 +1,14 @@
+package com.itacademy.AttendanceApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceAppApplication.class, args);
+    }
+
+}
