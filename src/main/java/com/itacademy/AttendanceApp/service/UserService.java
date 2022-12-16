b2c866ffad97cf821ce76f5ae15ddbf873ffd12b
Ahmed <ahmedheric2@gmail.com>
2022-12-16 01:18:21 +0100

    User service and User repository

---
+++ src/main/java/com/itacademy/AttendanceApp/service/UserService.java
@@ -0,0 +1,27 @@
+package com.itacademy.AttendanceApp.service;
+
+import com.itacademy.AttendanceApp.entity.User;
+import com.itacademy.AttendanceApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
