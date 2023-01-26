06af67ec122ea7c74fd2d6d589c488a89811e02e
SEJO <sead.bander@gmail.com>
2023-01-26 14:28:58 +0100

    port config

--- src/main/java/com/itacademy/AttendanceAppApplication.java
+++ src/main/java/com/itacademy/AttendanceAppApplication.java
@@ -4,6 +4,7 @@ package com.itacademy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
 
 
 @SpringBootApplication
@@ -15,6 +16,10 @@ public class AttendanceAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AttendanceAppApplication.class, args);
 
+        SpringApplication app = new SpringApplication(AttendanceAppApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8080"));
+        app.run(args);
     }
 
 }
