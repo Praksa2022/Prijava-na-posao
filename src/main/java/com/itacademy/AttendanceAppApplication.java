8af6ab601fa88c47e2951fc2e18b50cb3e3e6b65
SEJO <sead.bander@gmail.com>
2023-01-30 20:22:52 +0100

    port config2

--- src/main/java/com/itacademy/AttendanceAppApplication.java
+++ src/main/java/com/itacademy/AttendanceAppApplication.java
@@ -1,27 +1,37 @@
 package com.itacademy;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 import java.util.Collections;
 
 
 @SpringBootApplication
-public class AttendanceAppApplication {
+public class AttendanceAppApplication implements CommandLineRunner {
 
+    @Value("${server.port}")
+    private String port;
 
-
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
-        SpringApplication.run(AttendanceAppApplication.class, args);
 
         SpringApplication app = new SpringApplication(AttendanceAppApplication.class);
         app.setDefaultProperties(Collections
-                .singletonMap("server.port", "8080"));
+                .singletonMap("server.port", "port"));
         app.run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Port from @Value: " + port);
+    }
 }
 
 
