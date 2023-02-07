07b3bf0705b6f4c2c18ff3bbf4789e00c242f683
Sancho-Sa <sanel_r@hotmail.com>
2023-02-07 15:19:51 +0100

    Update TimeEntryController.java

--- src/main/java/com/itacademy/AttendanceApp/controller/TimeEntryController.java
+++ src/main/java/com/itacademy/AttendanceApp/controller/TimeEntryController.java
@@ -2,19 +2,12 @@ package com.itacademy.AttendanceApp.controller;
 
 import com.itacademy.AttendanceApp.entity.TimeEntry;
 
-import com.itacademy.AttendanceApp.entity.User;
-import com.itacademy.AttendanceApp.repository.UserRepository;
 import com.itacademy.AttendanceApp.service.TimeEntryService;
 
-import jakarta.persistence.NoResultException;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
@@ -22,32 +15,20 @@ import java.util.Optional;
 public class TimeEntryController {
     private final TimeEntryService timeEntryService;
 
-    private final UserRepository userRepository;
-
-    public TimeEntryController(TimeEntryService timeEntryService, UserRepository userRepository) {
+    public TimeEntryController(TimeEntryService timeEntryService) {
         this.timeEntryService = timeEntryService;
-        this.userRepository = userRepository;
     }
 
-    @PostMapping("/{id}/entry")
-    public ResponseEntity<Object> createTimeEntry(@PathVariable Long id, @Valid @RequestBody TimeEntry timeEntry){
-        Optional<User> user = userRepository.findById(id);
-
-        if (user.isEmpty())
-            throw new NoResultException("user does not exist");
-
-        timeEntry.setUser(user.get());
-        TimeEntry savedTimeEntry = timeEntryService.saveTimeEntry(timeEntry);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("{id}")
-                .buildAndExpand(savedTimeEntry.getId())
-                .toUri();
-        return ResponseEntity.created(location).build();
+    @PostMapping("/{username}/entry")
+    public TimeEntry createTimeEntry(@PathVariable String username, @RequestBody TimeEntry timeEntry){
+//        timeEntry.setUser(username);
+        timeEntry.setId(null);
+        return timeEntryService.saveTimeEntry(timeEntry);
     }
 
     @GetMapping("/{username}/entry")
-    public List<TimeEntry> getAllByUsername(@PathVariable String username){
-        return getAllByUsername(username);
+    public TimeEntry getAllByUser(@PathVariable String username){
+        return timeEntryService.getByUser(username);
     }
 
     @GetMapping("/")
@@ -55,14 +36,15 @@ public class TimeEntryController {
         return timeEntryService.getAllTimeEntry();
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable int id){
+    @DeleteMapping("/{username}/entry/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String username, @PathVariable int id){
         timeEntryService.deleteRow(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{username}/entry/{id}")
     public TimeEntry updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody TimeEntry timeEntry){
-        timeEntryService.save(timeEntry);
+        timeEntryService.saveTimeEntry(timeEntry);
         return timeEntry;
     }
 }
